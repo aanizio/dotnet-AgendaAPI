a60ab7eb9ebb052b18a5ba58f2fac50379080e6e
Anderson <Anderson Anizio>
2022-02-08 23:11:08 -0300

    feat: suporte a MySQL como db

--- AgendaAPI/Program.cs
+++ AgendaAPI/Program.cs
@@ -10,8 +10,28 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<ContatoContext>(opt =>
-    opt.UseInMemoryDatabase("Agenda"));
+// builder.Services.AddDbContext<ContatoContext>(opt =>
+//     opt.UseInMemoryDatabase("Agenda"));
+
+// Replace with your connection string.
+var connectionString = "server=localhost;user=root;password=1234;database=agenda";
+
+// Replace with your server version and type.
+// Use 'MariaDbServerVersion' for MariaDB.
+// Alternatively, use 'ServerVersion.AutoDetect(connectionString)'.
+// For common usages, see pull request #1233.
+var serverVersion = new MySqlServerVersion(new Version(8, 0, 27));
+
+// Replace 'YourDbContext' with the name of your own DbContext derived class.
+builder.Services.AddDbContext<ContatoContext>(
+    dbContextOptions => dbContextOptions
+        .UseMySql(connectionString, serverVersion)
+        // The following three options help with debugging, but should
+        // be changed or removed for production.
+        .LogTo(Console.WriteLine, LogLevel.Information)
+        .EnableSensitiveDataLogging()
+        .EnableDetailedErrors()
+);
 
 var app = builder.Build();
 
