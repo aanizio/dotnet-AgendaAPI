310b5e2acf7631a967b5dd5a29aad26c8a4a619e
Anderson <Anderson Anizio>
2022-02-09 08:36:29 -0300

    fix: connection strings to appsettings

--- AgendaAPI/Program.cs
+++ AgendaAPI/Program.cs
@@ -10,27 +10,28 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// builder.Services.AddDbContext<ContatoContext>(opt =>
-//     opt.UseInMemoryDatabase("Agenda"));
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .Build();
 
-// Replace with your connection string.
-var connectionString = "server=localhost;user=root;password=1234;database=agenda";
-
-// Replace with your server version and type.
-// Use 'MariaDbServerVersion' for MariaDB.
-// Alternatively, use 'ServerVersion.AutoDetect(connectionString)'.
-// For common usages, see pull request #1233.
-var serverVersion = new MySqlServerVersion(new Version(8, 0, 27));
-
-// Replace 'YourDbContext' with the name of your own DbContext derived class.
 builder.Services.AddDbContext<ContatoContext>(
-    dbContextOptions => dbContextOptions
-        .UseMySql(connectionString, serverVersion)
-        // The following three options help with debugging, but should
-        // be changed or removed for production.
-        .LogTo(Console.WriteLine, LogLevel.Information)
-        .EnableSensitiveDataLogging()
-        .EnableDetailedErrors()
+    dbContextOptions => 
+    {
+        var ConnectionStrings = configuration.GetValue<string>("ConnectionStrings:default");
+
+        // Replace with your server version and type.
+        // Use 'MariaDbServerVersion' for MariaDB.
+        // Alternatively, use 'ServerVersion.AutoDetect(connectionString)'.
+        // For common usages, see pull request #1233.
+        var serverVersion = new MySqlServerVersion(new Version(8, 0, 27));
+
+        dbContextOptions.UseMySql(ConnectionStrings, serverVersion)
+            // The following three options help with debugging, but should
+            // be changed or removed for production.
+            .LogTo(Console.WriteLine, LogLevel.Information)
+            .EnableSensitiveDataLogging()
+            .EnableDetailedErrors();
+    }
 );
 
 var app = builder.Build();
